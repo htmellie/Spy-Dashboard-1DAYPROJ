177e48289a60c98de3b1ef1a5362fec9a0f3787a
Ellie <ellieredmayne@hotmail.com>
2021-03-19 16:25:16 +0000

    bikes and missions

--- FrontEnd/bikes-and-bond/src/Components/BikesPage/index.js
+++ FrontEnd/bikes-and-bond/src/Components/BikesPage/index.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import useGet from "../../libs/useGet";
+import NavBar from "../NavBar";
+
+import DisplayBike from "../DisplayBike";
 
 function BikesPage() {
+  // Get all the misssions - useGet (URL/missions?search${SS}, [SS])
+  // Make a search state - SS
+  // Map data missions into mission components to display
+  // Nav Bar
+  const [search, setSearch] = useState("");
+  let url = `https://localhost:5001/bikes?search${search}`;
+  let bikes = useGet(url, [search]);
+  console.log(bikes);
+  if (!bikes) {
+    return <p>loading</p>;
+  }
   return (
     <div>
       <h1>Bikes</h1>
+      <NavBar />
+      <input
+        className="Bikes Box"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div>
+        {bikes.map((bike) => {
+          return <DisplayBike bike={bike} />;
+        })}
+      </div>
     </div>
   );
 }
--- FrontEnd/bikes-and-bond/src/Components/DashboardProfilePage/index.js
+++ FrontEnd/bikes-and-bond/src/Components/DashboardProfilePage/index.js
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
-import useGet from '../../libs/useGet';
-import { Bikes, Missions } from '../../libs/sampleData';
-import { useAuth0 } from '@auth0/auth0-react';
-import DisplayBike from '../DisplayBike';
-import Profile from '../Profile';
-import DisplayMission from '../DisplayMission';
+import { Link } from "react-router-dom";
+import useGet from "../../libs/useGet";
+import { Bikes, Missions } from "../../libs/sampleData";
+import { useAuth0 } from "@auth0/auth0-react";
+import DisplayBike from "../DisplayBike";
+import Profile from "../Profile";
+import DisplayMission from "../DisplayMission";
+import NavBar from "../NavBar";
 
 function DashboardProfilePage(props) {
   const { user, logout, isAuthenticated, isLoading } = useAuth0();
@@ -19,15 +20,15 @@ function DashboardProfilePage(props) {
   return (
     isAuthenticated && (
       <>
-        
         <Profile
           name={user.name}
           picture={user.picture}
           email={user.email}
           logout={logout}
         />
-        <DisplayBike bike={Bikes[0]} heading={'Recommended Bike'} />
-        <DisplayMission mission={Missions[0]} heading={'Recommended Mission'} />
+        <DisplayBike bike={Bikes[0]} heading={"Recommended Bike"} />
+        <DisplayMission mission={Missions[0]} heading={"Recommended Mission"} />
+        <NavBar />
       </>
     )
   );
