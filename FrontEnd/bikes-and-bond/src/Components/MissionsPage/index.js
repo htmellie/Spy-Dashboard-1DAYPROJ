1c1c96bfa424e59f7572f06f768fbdd0cc0570ff
craig2812 <craig2812@gmail.com>
2021-03-19 15:39:47 +0000

    Branch off for mission page dev

--- FrontEnd/bikes-and-bond/src/Components/MissionsPage/index.js
+++ FrontEnd/bikes-and-bond/src/Components/MissionsPage/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 function MissionsPage() {
+  // Get all the misssions - useGet (URL/missions?search${SS}, [SS])
+  // Make a search state - SS 
+  // Map data missions into mission components to display 
+  // Nav Bar 
+
   return (
     <div>
       <h1>Missions</h1>
+      <input className={} value={search} onChange={(e)=>(setSearch(e.target.value))}></input>
+      <div className></div>
+      
+
     </div>
   );
 }
